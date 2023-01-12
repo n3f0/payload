12f840f18e6ec67efd155c3465f8390f121aa3be
n3f0 <nefo424@gmail.com>
2023-01-12 21:05:07 +0700

    Create ls.php

---
+++ ls.php
@@ -0,0 +1,13 @@
+$a=$_GET['dir'];
+if ($handle = opendir($a)) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
